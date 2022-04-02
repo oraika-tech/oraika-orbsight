6022912dd6f53ceeadd382d7642d76de6e6d8cf6
Girish Patel <girishpatel.bits@gmail.com>
2022-04-03 00:07:57 +0530

    Changes to autostart metabase on machine start (#18)
    Co-authored-by: Lalit Pagaria <pagaria.lalit@gmail.com>

--- backend/processing/metabase/metabase_local_tunnel.sh
+++ backend/processing/metabase/metabase_local_tunnel.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-REMOTE_PORT=8080
-LOCAL_PORT=7070
-SSH_PK_FILE=~/.ssh/id_rsa_aws
-USER=ec2-user
-HOST=ec2-54-77-212-91.eu-west-1.compute.amazonaws.com
+REMOTE_PORT=${METABASE_REMOTE_PORT:-80}
+HOST=${METABASE_HOST:-ec2-34-253-228-40.eu-west-1.compute.amazonaws.com}
 
-ssh -N -L $LOCAL_PORT:localhost:$REMOTE_PORT -i $SSH_PK_FILE $USER@$HOST &
+LOCAL_PORT=${LOCAL_PORT:-7070}
+SSH_PK_FILE=${SSH_PK_FILE:-~/.ssh/id_rsa_aws}
+USER=ec2-user
+
+ssh -N -L $LOCAL_PORT:localhost:"$REMOTE_PORT" -i $SSH_PK_FILE $USER@"$HOST" &
 disown
 
 echo Access Metabase: http://localhost:7070/
