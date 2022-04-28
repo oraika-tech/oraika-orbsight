5523b09aa8daeccd4eae9ee53fd98ead364fc0f4
Girish Patel <girishpatel.bits@gmail.com>
2022-04-28 16:04:19 +0530

    ORB-62 Removed spaces from entity tsv and added limit count per observer type in cron (#33)

--- backend/processing/cron/active_observer_publisher.sh
+++ backend/processing/cron/active_observer_publisher.sh
@@ -13,6 +13,10 @@ DB_USER=${DB_USER:-obsights}
 DB_PASSWORD=${DB_PASSWORD:-obsights}
 DB_HOST=${DB_HOST:-localhost:5432}
 LOOKUP_PERIOD=${LOOKUP_PERIOD:-7m}
+TWITTER_LIMIT_COUNT=${TWITTER_LIMIT_COUNT:-100}
+ANDROID_LIMIT_COUNT=${ANDROID_LIMIT_COUNT:-20}
+IOS_LIMIT_COUNT=${IOS_LIMIT_COUNT:-100}
+LIMIT_COUNT=${LIMIT_COUNT:-20}
 
 MSG_FILE=/tmp/observer_rows.json
 
@@ -23,6 +27,12 @@ SELECT row_to_json(ob) FROM (
          e.identifier as entity_identifier, e.simple_name as entity_simple_name, e.regulated_type,
          o.data->'url' as app_url, o.data->'official_handle' as twitter_handle,
          '$LOOKUP_PERIOD' as lookup_period,
+         CASE
+         WHEN o.observer_type = 1 THEN $TWITTER_LIMIT_COUNT
+         WHEN o.observer_type = 2 THEN $ANDROID_LIMIT_COUNT
+         WHEN o.observer_type = 3 THEN $IOS_LIMIT_COUNT
+         ELSE $LIMIT_COUNT
+         END as limit_count,
          e.country as entity_country, e.city as entity_city
  FROM observer o JOIN entity e on o.entity_id = e.identifier
  WHERE o.is_enabled = true and e.is_enabled = true
--- backend/processing/cron/cron_deploy.sh
+++ backend/processing/cron/cron_deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+DIR_PATH=$(dirname "$0")
 mkdir -p ~/.obsights/cron/
-sudo cp obsights/processing/cron/active_observer_publisher.sh ~/.obsights/cron/
-sudo cp obsights/processing/cron/observer_messager_job.crontab /etc/cron.d/observer_messager_job.crontab
+sudo cp $DIR_PATH/active_observer_publisher.sh ~/.obsights/cron/
+sudo cp $DIR_PATH/observer_messager_job.crontab /etc/cron.d/observer_messager_job.crontab
