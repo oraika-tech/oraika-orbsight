ac243e29fe1e04863c7c7c1d16a40d3775eabe91
Girish Patel <girishpatel.bits@gmail.com>
2022-12-14 20:04:53 +0530

    Added cron for event time rotation and data changes for demo accounts (#111)

--- backend/processing/cron/cron_deploy.sh
+++ backend/processing/cron/cron_deploy.sh
@@ -4,3 +4,5 @@ DIR_PATH=$(dirname "$0")
 mkdir -p ~/.orbsight/cron/
 sudo cp $DIR_PATH/active_observer_publisher.sh ~/.orbsight/cron/
 sudo cp $DIR_PATH/observer_messager_job.crontab /etc/cron.d/observer_messager_job.crontab
+sudo cp $DIR_PATH/demo_event_time_rotator.sh ~/.orbsight/cron/
+sudo cp $DIR_PATH/event_time_update_job.crontab /etc/cron.d/event_time_update_job.crontab
